6e034284a78db4d69c1c5c0ef43ba865d67ed7f9
thamanb <brettt132@gmail.com>
2021-12-13 17:10:25 -0500

    admin on project page

--- project.php
+++ project.php
@@ -147,6 +147,16 @@
             </div>
         </div>
     </div>
+    <?php
+        if($_SESSION['isAdmin'] && $_SESSION['userID']==$project['user_ID']){
+    ?>
+    <div class="container p-3">
+        <button type="button" class="btn btn-info"><a href="modify.php?entity=project&projectid=<?=$id?>" style="text-decoration: none; color: white;">Modify</a></button>
+        <button type="button" class="btn btn-danger"><a href="delete.php?entity=project&projectid=<?=$id?>" style="text-decoration: none; color: white;">Delete</a></button>
+    </div>
+    <?php
+        }
+    ?>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
